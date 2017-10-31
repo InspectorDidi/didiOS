8d78c330179e3c9f4ecd89bdaff2567d709eec7d
Thomas Graves <thomas@ooo.pm>
2017-10-31 13:53:13 -0400

    Jump to kmain in libdidios

--- src/lib.rs
+++ src/lib.rs
@@ -7,6 +7,17 @@ extern fn eh_personality() {
 
 #[lang = "panic_fmt"]
 extern fn rust_begin_panic() -> ! {
-    loop {}
+    loop { }
+}
+
+#[no_mangle]
+pub extern fn kmain() -> ! {
+    unsafe {
+        let vga = 0xb8000 as *mut u64;
+
+        *vga = 0x2f592f412f4b2f4f;
+    };
+
+    loop { }
 }
 
